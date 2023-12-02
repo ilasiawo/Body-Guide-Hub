5e4f64c810906e2f6aee4e3b25ea93a88eb3b434
ilasiawo <owais.ali.106902@gmail.com>
2023-12-02 18:08:21 +0530

    final commit for the project

---
+++ src/utils/fetchData.js
@@ -0,0 +1,23 @@
+
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '05aee5f7c4msh74e68b5daf5bb01p10c4b8jsne1f42edc4ce5',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '05aee5f7c4msh74e68b5daf5bb01p10c4b8jsne1f42edc4ce5',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
